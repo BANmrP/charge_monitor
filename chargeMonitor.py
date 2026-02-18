aaa2189624a0741c6d839105a056620c8eb1a6c8
BANmrP <ukrmaper@gmail.com>
2026-02-18 22:13:28 +0200

    bag fix
    bag fix

--- chargeMonitor.py
+++ chargeMonitor.py
@@ -10,15 +10,15 @@ while True:
 	battery = psutil.sensors_battery()
 	percent = battery.percent
 
-	if percent <= 10 and percent <= last_percent:
+	if percent <= 10 and percent != last_percent:
 		toast('Заряд 10%			')
 		last_percent = percent
 
-	elif percent <= 20 and percent <= last_percent:
+	elif percent <= 20 and percent != last_percent:
 		toast('Заряд 20%			')
 		last_percent = percent
 
-	elif percent <= 30 and percent <= last_percent:
+	elif percent <= 30 and percent != last_percent:
 		toast('Заряд 30%			')
 		last_percent = percent
 
