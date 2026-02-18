a208eb089e59e3bad68af3b6dbfb86a9239aae46
BANmrP <ukrmaper@gmail.com>
2026-02-18 21:37:01 +0200

    first commit
    first commit

---
+++ chargeMonitor.py
@@ -0,0 +1,26 @@
+import psutil
+from win11toast import toast
+import time
+
+last_percent = None
+
+while True:
+	time.sleep(60)
+
+	battery = psutil.sensors_battery()
+	percent = battery.percent
+
+	if percent <= 10 and percent <= last_percent:
+		toast('Заряд 10%			')
+		last_percent = percent
+
+	elif percent <= 20 and percent <= last_percent:
+		toast('Заряд 20%			')
+		last_percent = percent
+
+	elif percent <= 30 and percent <= last_percent:
+		toast('Заряд 30%			')
+		last_percent = percent
+
+	elif percent > last_percent:	#charging test
+		last_percent = None
